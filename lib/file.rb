532b8c0b8ee80fcd63171a721b05540b27f92ce9
Louis Pilfold <louis@lpil.uk>
2015-12-11 23:28:02 +0000

    File fun!

--- lib/file.rb
+++ lib/file.rb
@@ -3,13 +3,24 @@ require_relative 'carhop'
 class Carhop::File
   attr_reader :path
 
-  def initialize(path)
+  def initialize(path:)
     @path = path
   end
 
-  def hash(path)
-    # Is there a way to stream this rather than loading the
-    # entire file into memory?
+  def hash
+    # Is there a way to stream this into the SHA function
+    # rather than loading the entire file into memory?
     @_hash ||= Digest::SHA256.hexdigest File.read(path)
+  end
+
+  def clean_name
+    @clean_name ||= File.basename(path)
+                    .downcase
+                    .gsub(/[^a-z_.]/, '-')
+                    .gsub(/-+/, '-')
+  end
+
+  def s3_name
+    "#{hash}-#{clean_name}"
   end
 end
--- spec/carhop/file_spec.rb
+++ spec/carhop/file_spec.rb
@@ -2,12 +2,39 @@ require_relative '../../lib/file'
 
 describe Carhop::File do
   let(:path)     { 'spec/fixtures/one.txt' }
-  let(:instance) { described_class.new path }
+  let(:instance) { described_class.new path: path }
+  let(:sha256) do
+    'edd1eae3d9703439752a14e742afd563739988fe057ff10843cc61542065e3b1'
+  end
 
   it { expect(instance.path).to eq path }
 
   it 'can report its sha256' do
-    sha = 'edd1eae3d9703439752a14e742afd563739988fe057ff10843cc61542065e3b1'
-    expect(instance.hash path).to eq sha
+    expect(instance.hash).to eq sha256
+  end
+
+
+  describe '#clean_name' do
+    it 'gets the base name from the path' do
+      file = described_class.new path: 'foo/bar.md'
+      expect(file.clean_name).to eq 'bar.md'
+    end
+
+    it 'strips all those horrible non internetty chars' do
+      file = described_class.new path: 'my stuff/what!"£^&*()?_-][#the.tar.gz'
+      expect(file.clean_name).to eq 'what-_-the.tar.gz'
+    end
+
+    it 'is always lowercase' do
+      file = described_class.new path: 'FOO.PNG'
+      expect(file.clean_name).to eq 'foo.png'
+    end
+  end
+
+
+  describe '#s3_name' do
+    it 'is the hash + the clean name!' do
+      expect(instance.s3_name).to eq "#{sha256}-one.txt"
+    end
   end
 end
